dbe58e0d52b00ec9ba639fa855f2603db8982775
Eduardo Perottoni <edu.perottoni@gmail.com>
2021-03-17 11:10:14 -0300

    Finalizando os exercícios sobre condicionais simples e compostos

---
+++ exercises/ex009.js
@@ -0,0 +1,11 @@
+var país = 'França'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil'){
+    console.log('Você é Brasileiro!')
+}else{
+    console.log('Você é Estrangeiro!')
+}
+
+/* Pode-se usar o operador ternário, neste caso
+var tipo = (país=='Brasil'?'Brasileiro!':'Estrangeiro!')
+console.log(tipo)*/
