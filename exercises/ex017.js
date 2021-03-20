c62a2aae3f77802ddfbd95c683f530ed233beda6
Eduardo Perottoni <edu.perottoni@gmail.com>
2021-03-20 08:50:04 -0300

    Finalizando aulas 13 e 14 - Repetições

---
+++ exercises/ex017.js
@@ -0,0 +1,17 @@
+var i = 1
+while (i<=6){
+    console.log(`${i}`)
+    i++
+}
+
+var cont = 1;
+do{
+    console.log(`${cont}`)
+    cont ++
+}while(cont<=6)
+
+var cont2 = 3;
+
+for (var c = 1; c<=6; c++){
+    console.log(c)
+}
