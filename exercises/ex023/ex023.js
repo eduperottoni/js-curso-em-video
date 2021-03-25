40fa2fe031793f446ce9bff4580baf266a2e2ad9
Eduardo Perottoni <edu.perottoni@gmail.com>
2021-03-25 20:46:36 -0300

    Finalizando o exercício final do curso - Analisador de números

--- exercises/ex023/ex023.js
+++ exercises/ex023/ex023.js
@@ -3,6 +3,8 @@ const numberInput = document.getElementById('numtxt')
 const addButton = document.querySelector('#add-button')
 const previewSelect = document.querySelector('#numlist')
 const removeButton = document.getElementById('remove-button')
+const analiseButton = document.querySelector('#analise-button')
+const resDiv = document.getElementById('res')
 
 
 var array = []
@@ -25,10 +27,12 @@ function validation(){ //Se o número for inválido, a função retorna 0. Caso 
 }
 
 function addToList(){
+    resDiv.innerHTML = ''
     var number = validation()
     if (number != 0){ //Testa se o número é válido
         previewSelect.style.display = 'inherit'
         removeButton.style.display = 'inherit'
+        analiseButton.style.display = 'inherit'
         var option = document.createElement('option')
         option.className = 'number-listed'
         option.value = number
@@ -36,7 +40,8 @@ function addToList(){
         previewSelect.appendChild(option)
         array.push(number)
     }
-     
+    numberInput.value = ''
+    numberInput.focus()
 }
 
 function removeList(){
@@ -44,7 +49,39 @@ function removeList(){
     previewSelect.innerHTML = ''
     previewSelect.style.display = 'none'
     removeButton.style.display = 'none'
+    analiseButton.style.display = 'none'
 }
+
+function analise(){
+    resDiv.innerHTML = ''
+    var arrayLength = array.length
+    if (arrayLength <= 1){
+        alert('A LISTA DEVE TER 2 OU MAIS VALORES PARA SER ANALISADA!')
+    }else{
+        var arraySorted = array.sort(function(a,b){
+            return a - b;
+        })
+        var arraySum = array.reduce((arraySum, num) => arraySum + num, 0)
+
+        var analiseTitle = document.createElement('h2')
+        analiseTitle.id = 'res-h2'
+        analiseTitle.innerHTML ='Análise da lista'
+        resDiv.appendChild(analiseTitle)
+
+        var resP = document.createElement('p')
+        resP.className = 'res-item'
+        resP.innerHTML = `Ao todo, temos <strong>${arrayLength} números cadastrados</strong></br></br>`
+        resP.innerHTML += `O <strong>maior valor</strong> informado foi <strong>${arraySorted[arrayLength-1]}</strong></br></br>`
+        resP.innerHTML += `O <strong>menor valor</strong> informado foi <strong>${arraySorted[0]}</strong></br></br>`
+        resP.innerHTML += `<strong>Somando</strong> todos os valores, temos <strong>${arraySum}</strong></br></br>`
+        resP.innerHTML += `A <strong>média</strong> dos valores da lista é <strong>${arraySum/arrayLength}</strong`
+        resDiv.appendChild(resP)
+    }
+}
+
+analiseButton.addEventListener('click', () =>{
+    analise()
+})
 
 addButton.addEventListener('click', () =>{
     addToList()
