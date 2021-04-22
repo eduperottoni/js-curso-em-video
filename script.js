3e633ceff0d599c7727873d1ecd61079e7879689
Eduardo Perottoni <edu.perottoni@gmail.com>
2021-04-22 09:00:11 -0300

    Aperfeiçoando o menu

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const menu = document.querySelector('nav.menu')
 const menuOptions = document.querySelector('nav.menu section.menu-options')
 
 function openCloseMenu(){
-    if (menu.classList == 'menu menu-closed'|| menu.classList == 'menu'){
+    if (menu.classList == 'menu' || menu.classList == 'menu menu-closed'){
         menu.classList = 'menu menu-opened'
         arrowSection.innerHTML = '<img src="./images/left-arrow-black.png" alt="Passe o Mouse e veja o menu">'
+        menuOptions.classList = 'menu-options menu-opened'
     }else{
-        menu.classList = 'menu'
+        menu.classList = 'menu menu-closed'
         arrowSection.innerHTML = '<img src="./images/rigth-arrow-black.png" alt="Passe o Mouse e veja o menu">'
+        menuOptions.classList = 'menu-options menu-closed'
     }
 }
 
