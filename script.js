9d6bc3aca893db8ee73640f3c2585df0f7ba561a
Eduardo Perottoni <edu.perottoni@gmail.com>
2021-04-21 23:05:26 -0300

    Trabalhando com a funcionalidade de menu dinâmico

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 const arrowSection = document.querySelector('section.menu-arrow')
 const menu = document.querySelector('nav.menu')
-
-function openMenu(){
-    menu.classList.add('menu-opened')
-}
-
-function closeMenu(){
-    menu.classList.remove('menu-opened')
-    menu.classList.add('menu-closed')
-}
+const menuOptions = document.querySelector('nav.menu section.menu-options')
