d57b4fd8aca4292aa2a752d0fbee7433f9d398b3
Eduardo Perottoni <edu.perottoni@gmail.com>
2021-04-22 09:39:06 -0300

    Estilizando as opções do menu

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//FUNCIONALIDADES DO MENU LATERAL DINÂMICO
 const arrowSection = document.querySelector('section.menu-arrow')
 const menu = document.querySelector('nav.menu')
 const menuOptions = document.querySelector('nav.menu section.menu-options')
