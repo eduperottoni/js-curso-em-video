873a499ec74d8f1de8d1516ac10f0c7feaa1ef6c
Eduardo Perottoni <edu.perottoni@gmail.com>
2021-04-23 03:03:16 -0300

    Finalizando´estilização padrão das páginas de conteúdo

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //FUNCIONALIDADES DO MENU LATERAL DINÂMICO
 const arrowSection = document.querySelector('section.menu-arrow')
 const menu = document.querySelector('nav.menu')
-const menuOptions = document.querySelector('nav.menu section.menu-options')
+const main = document.querySelector('main.content')
 
-function openCloseMenu(){
-    if (menu.classList == 'menu' || menu.classList == 'menu menu-closed'){
-        menu.classList = 'menu menu-opened'
-        arrowSection.innerHTML = '<img src="./images/left-arrow-black.png" alt="Passe o Mouse e veja o menu">'
-        menuOptions.classList = 'menu-options menu-opened'
+function showHideMenu(){
+    if (menu.classList == 'menu' || menu.classList == 'menu closed'){
+        menu.classList = 'menu opened'
+        arrowSection.classList = 'menu-arrow opened'
+        main.classList = 'content min'
     }else{
-        menu.classList = 'menu menu-closed'
-        arrowSection.innerHTML = '<img src="./images/rigth-arrow-black.png" alt="Passe o Mouse e veja o menu">'
-        menuOptions.classList = 'menu-options menu-closed'
+        menu.classList = 'menu closed'
+        arrowSection.classList = 'menu-arrow closed'
+        main.classList = 'content max'
     }
 }
 
@@ -22,11 +22,9 @@ function openCloseClassBox(value){
     for (i in classBox){
         if (classBox[i].id == value){
             if (classBox[i].classList == 'class-box invisible'){
-                classBox[i].classList.remove('invisible')
-                classBox[i].classList.add('visible')
+                classBox[i].classList = 'class-box visible'
             }else{
-                classBox[i].classList.remove('visible')
-                classBox[i].classList.add('invisible')
+                classBox[i].classList = 'class-box invisible'
             }
         }
     }
